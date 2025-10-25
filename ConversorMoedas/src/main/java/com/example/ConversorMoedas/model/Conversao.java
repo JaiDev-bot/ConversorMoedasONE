d1f0d346a8b7776c34973555b099afae3c77ca29
Jaiane <jaianesoares700@gmail.com>
2025-10-25 12:48:32 -0300

    feat:feito model e configurações iniciais

---
+++ ConversorMoedas/src/main/java/com/example/ConversorMoedas/model/Conversao.java
@@ -0,0 +1,11 @@
+package com.example.ConversorMoedas.model;
+
+public record Conversao(String result,
+                        String base_code,
+                        String target_code,
+                        double conversion_rate){
+
+
+
+
+}
---
+++ ConversorMoedas/src/main/java/com/example/ConversorMoedas/service/ConsultaCambio.java
@@ -0,0 +1,4 @@
+package com.example.ConversorMoedas.service;
+
+public class ConsultaCambio {
+}
